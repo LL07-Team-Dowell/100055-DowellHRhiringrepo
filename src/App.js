55b5aaf317c24351f25e7514008292c505ab5654
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-25 12:18:21 +0600

    initiated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
+
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Home />
+      <Auth/>
+    </Container>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import { Container, Grow} from '@material-ui/core';
+
+const Home = () => {
+
+  return (
+    <Grow in>
+      <Container>
+        <h3>HOME PAGE</h3>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
