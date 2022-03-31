6e6f80046fceb0ee8f317316f0f1aa8d65725320
George Kibe <georgekibewambui@gmail.com>
2022-03-31 19:29:52 +0300

    Files configuration

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 
-def index (request):
+
+def index(request):
     return HttpResponseRedirect(reverse('JobPortal_App:job_list'))
