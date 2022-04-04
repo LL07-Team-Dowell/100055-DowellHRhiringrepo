a96edce613d86432f37b08cb0a1880f9fa3b001f
anjanadowell <68542579+anjanadowell@users.noreply.github.com>
2022-04-04 13:09:21 +0530

    Delete jobportal_app directory

--- jobportal_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CreateJobs, JobApplication
-
-# Register your models here.
-admin.site.register(CreateJobs)
-admin.site.register(JobApplication)
--- jobportal_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobportalAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobportal_app'
