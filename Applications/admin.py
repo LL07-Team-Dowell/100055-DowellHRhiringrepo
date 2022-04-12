8ab62ff1be60174ff9f7300116331e11f4ef5e64
George Kibe <georgekibewambui@gmail.com>
2022-04-12 10:01:31 +0300

    renaming and configuring serializers

--- Applications/admin.py
+++ Applications/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import JobApplication,NewJobsApplication,HRCanidateStatus , TMCanidateStatus , AccountsCanidateStatus, InternetSpeed
+from .models import JobApplication, NewJobsApplication, HRCandidateStatus, TMCandidateStatus, AccountsCandidateStatus, InternetSpeed
 
 admin.site.register(JobApplication)
 admin.site.register(NewJobsApplication)
-admin.site.register(HRCanidateStatus)
-admin.site.register(AccountsCanidateStatus)
-admin.site.register(TMCanidateStatus)
+admin.site.register(HRCandidateStatus)
+admin.site.register(AccountsCandidateStatus)
+admin.site.register(TMCandidateStatus)
 admin.site.register(InternetSpeed)
--- Applications/models.py
+++ Applications/models.py
@@ -2,41 +2,52 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class JobApplication(models.Model):
     id = models.AutoField(primary_key=True)
-    feedBack = models.TextField(null = True)
-    freelancePlatformUrl = models.URLField(null = True)
-    applicationName = models.CharField(max_length = 132)
-    status = models.CharField(max_length = 132, null = True,default="Pending")
-    applicationlvl = models.CharField(max_length = 132, null = True,default="HR")
-    country = models.CharField(max_length = 132,null = True)
-    username = models.CharField(max_length = 132,null = True)
-    qualification = models.CharField(max_length = 132,null = True)
-    freelancePlatform = models.CharField(max_length = 132,null = True)
-    user = models.ForeignKey(User , on_delete = models.CASCADE,null = True)
+    feedBack = models.TextField(null=True)
+    freelancePlatformUrl = models.URLField(null=True)
+    applicationName = models.CharField(max_length=132)
+    status = models.CharField(max_length=132, null=True, default="Pending")
+    applicationlvl = models.CharField(max_length=132, null=True, default="HR")
+    country = models.CharField(max_length=132, null=True)
+    username = models.CharField(max_length=132, null=True)
+    qualification = models.CharField(max_length=132, null=True)
+    freelancePlatform = models.CharField(max_length=132, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     others = models.JSONField()
 
     def __str__(self):
         return self.applicationName
 
-class InternetSpeed(models.Model):
-    #  user = models.ForeignKey(User , on_delete = models.CASCADE,null = True)
-     user = models.CharField(max_length = 24,null = True)
-     download_speed = models.CharField(max_length = 24,null = True)
-     upload_speed = models.CharField(max_length = 24,null = True)
+    class Meta:
+        verbose_name_plural = 'JobApplications'
 
-     def __str__(self):
+
+class InternetSpeed(models.Model):
+
+    #  user = models.ForeignKey(User , on_delete = models.CASCADE,null = True)
+    user = models.CharField(max_length=24, null=True)
+    download_speed = models.CharField(max_length=24, null=True)
+    upload_speed = models.CharField(max_length=24, null=True)
+
+    def __str__(self):
         return self.user
 
+    class Meta:
+        verbose_name_plural = 'Internet Speeds'
+
+
 class NewJobsApplication(models.Model):
-    feedBack = models.TextField(null = True)
-    freelancePlatformUrl = models.URLField(null = True)
-    applicationName = models.CharField(max_length = 132)
-    status = models.CharField(max_length = 132, null = True)
-    country = models.CharField(max_length = 132,null = True)
-    username = models.CharField(max_length = 132,null = True)
-    qualification = models.CharField(max_length = 132,null = True)
-    freelancePlatform = models.CharField(max_length = 132,null = True)
+    feedBack = models.TextField(null=True)
+    freelancePlatformUrl = models.URLField(null=True)
+    applicationName = models.CharField(max_length=132)
+    status = models.CharField(max_length=132, null=True)
+    country = models.CharField(max_length=132, null=True)
+    username = models.CharField(max_length=132, null=True)
+    qualification = models.CharField(max_length=132, null=True)
+    freelancePlatform = models.CharField(max_length=132, null=True)
     others = models.JSONField()
     General_Terms_Conditions = models.JSONField()
     Technical_Specifications = models.JSONField()
@@ -46,24 +57,41 @@ class NewJobsApplication(models.Model):
     def __str__(self):
         return self.applicationName
 
-class HRCanidateStatus(models.Model):
+    class Meta:
+        verbose_name_plural = 'New JobApplications'
+
+
+class HRCandidateStatus(models.Model):
     id = models.AutoField(primary_key=True)
-    hr_Status = models.CharField(max_length = 132, null = True)
-    hr_FeedBack = models.CharField(max_length = 300, null = True)
-    JobApplication = models.ForeignKey(JobApplication , on_delete = models.CASCADE,null = True)
+    hr_Status = models.CharField(max_length=132, null=True)
+    hr_FeedBack = models.CharField(max_length=300, null=True)
+    JobApplication = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.JobApplication.applicationName
 
-class TMCanidateStatus(models.Model):
-    id = models.AutoField(primary_key=True)
-    team_lead_status = models.CharField(max_length = 132, null = True)
-    team_lead_feedback = models.CharField(max_length = 300 , null = True)
-    JobApplication = models.ForeignKey(JobApplication , on_delete = models.CASCADE,null = True)
+    class Meta:
+        verbose_name_plural = 'HR CandidateStatuses'
 
-class AccountsCanidateStatus(models.Model):
-    id = models.AutoField(primary_key=True)
-    accounts_status = models.CharField(max_length = 132 , null = True)
-    accounts_feedback = models.CharField(max_length = 132,null = True)
-    JobApplication = models.ForeignKey(JobApplication , on_delete = models.CASCADE,null = True)
 
+class TMCandidateStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    team_lead_status = models.CharField(max_length=132, null=True)
+    team_lead_feedback = models.CharField(max_length=300, null=True)
+    JobApplication = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name_plural = 'TMCandidateStatuses'
+
+
+class AccountsCandidateStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    accounts_status = models.CharField(max_length=132, null=True)
+    accounts_feedback = models.CharField(max_length=132, null=True)
+    JobApplication = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name_plural = 'AccountsCandidateStatuses'
